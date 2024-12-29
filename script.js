696d4643b1ab8389c0f8aa73b74d4d6f720d3469
Shreyash Kant <108665847+Shreyash-kant@users.noreply.github.com>
2024-12-29 15:08:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const makeChange = (c) => {
   // your name here
-};
+	const obj= {};
+	obj.q = parseInt(c/25);
+	c = c%25;
+	obj.d = parseInt(c/10);
+	c = c%10;
+	obj.n = parseInt(c/5);
+	c = c%5;
+	obj.p = parseInt(c);
+	return obj;
+}; 
 
 // Do not the change the code below
-const c = prompt("Enter c: ");
+const c = parseInt(prompt("Enter c: "));
 alert(JSON.stringify(makeChange(c)));
